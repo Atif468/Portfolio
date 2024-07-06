5de018bba9baa46a845fc497ea61414149e45778
Mohd Atif Ansari <114794817+Atif468@users.noreply.github.com>
2024-07-07 00:50:57 +0530

    Update Introduction.jsx

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -42,7 +42,7 @@ function Introduction() {
         </p>
         <a
           className="text-center inline-block py-2 h-12 w-32 border border-white rounded-full"
-          href="https://raw.githubusercontent.com/your-username/your-repository/main/RESUME.pdf"
+          href="https://raw.githubusercontent.com/Atif468/Portfolio/main/RESUME.pdf"
           download="RESUME.pdf"
           >
           Resume
