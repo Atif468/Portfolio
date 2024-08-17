69b9d11e9d0d1b3f20047ed81e9bbbe0e36c80a9
Mohd Atif Ansari <114794817+Atif468@users.noreply.github.com>
2024-08-17 22:52:04 +0530

    Update Introduction.jsx

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -9,14 +9,14 @@ function Introduction() {
   const splitter = new GraphemeSplitter();
   return (
     <section
-      className="relative text-white font-sans min-h-screen flex items-center justify-center"
+      className="text-white font-sans min-h-screen flex justify-center items-center"
       data-aos="fade-down-right"
     >
       {
         console.log("hello")
       }
       <Particle />
-      <div className=" overflow-hidden  m-10">
+      <div className="z-10 m-10">
         <h1 className="text-5xl font-bold mb-6">
           Hello, I'm
           <span style={{ fontSize: "1em", color: "white" }}>
